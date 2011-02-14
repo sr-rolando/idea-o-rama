2eeff8effb5079520f9c5da8e9e3e4530bb2dec3
gchq <github@gasbottle-county.eu>
2011-02-14 23:02:29 +0100

    Add foreign key column before being able to put a fk constraint on it.

--- db/migrate/20110214205407_add_idea_topic_reference.rb
+++ db/migrate/20110214205407_add_idea_topic_reference.rb
@@ -1,13 +1,14 @@
 class AddIdeaTopicReference < ActiveRecord::Migration
   def self.up
-    change_table ideas do |t|
+    change_table :ideas do |t|
+      t.integer "topic_id"
       # uses the 'foreigner' gem:
       t.foreign_key :topics, :dependent => :delete
     end
   end
 
   def self.down
-    change_table ideas do |t|
+    change_table :ideas do |t|
       t.remove :topics_id
     end
   end
