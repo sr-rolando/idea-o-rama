595cd94c3afc1b0187c2f6dea3a9b964b7f23423
gchq <github@gasbottle-county.eu>
2011-02-14 22:30:58 +0100

    Re-add some renamed migration file.

---
+++ db/migrate/20110214205407_add_idea_topic_reference.rb
@@ -0,0 +1,14 @@
+class AddIdeaTopicReference < ActiveRecord::Migration
+  def self.up
+    change_table ideas do |t|
+      # uses the 'foreigner' gem:
+      t.foreign_key :topics, :dependent => :delete
+    end
+  end
+
+  def self.down
+    change_table ideas do |t|
+      t.remove :topics_id
+    end
+  end
+end
